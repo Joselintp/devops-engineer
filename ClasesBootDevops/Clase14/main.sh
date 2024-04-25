4b932ca2aa22b9af39b9b1b2a708f1f446d76356
Joselintp <161532926+Joselintp@users.noreply.github.com>
2024-04-24 22:10:15 -0400

    Create main.sh

---
+++ ClasesBootDevops/Clase14/main.sh
@@ -0,0 +1,58 @@
+ #!/bin/bash
+
+ # Función para mostrar el título: Información del sistema seguido del nombre del nombre de usuario
+ function system_info() {
+   echo "*** Informe del Sistema para: $(hostname) ***"
+ }
+
+ # Función para mostrar Fecha / Hora
+ function date_and_time() {
+   date +"%d/%m/%Y %H:%M:%S"
+ }
+
+ # Función para mostrar uso de disco
+ function disk_usage() {
+   # df -h: muestra el espacio disponible en el sistema de archivos
+   # grep '/': filtra las líneas de salida que contienen la barra diagonal "/"
+   # tail -n 1: se utiliza para mostrar la información de la partición raíz 
+   # awk '{print $5}': Este comando extrae la columna 5 del resultado 
+   df -h | grep '/' | tail -n 1 | awk '{print $5}'
+ }
+
+ # Función para mostrar usuarios conectados
+ function logged_users() {
+   who
+ }
+
+ # Función para mostrar el uso de memoria en Megas
+ function memory_usage() {
+ # free: muestra información sobre el uso de memoria del sistema
+ # awk permite procesar y manipular texto.
+ free -m | awk 'NR==2{printf "Total: %sMB - Disponible: %sMB\n", $2, $7}'
+ }
+
+ # Función para mostrar los procesos que coincidan con el nombre indicado
+ function display_processes() {
+   if [ -z "$1" ]; then
+     echo "No se especificó ningún nombre de proceso"
+   else
+     ps -aux | grep $1
+   fi
+ }
+
+main(){
+ # Función principal
+ system_info
+ echo ""
+ echo "Fecha y Hora: $(date_and_time)"
+ echo "Uso del Disco: $(disk_usage)"
+ echo "Usuarios Logueados: $(logged_users)"
+ echo "Uso de Memoria: $(memory_usage)"
+ echo "Procesos: "
+ read -p "Ingrese un nombre o parte del nombre de un proceso: " process_name
+ display_processes $process_name
+ echo ""
+ echo "Script ejecutado con éxito"
+}
+
+main $1
