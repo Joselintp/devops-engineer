1e98ee3c37d627847e76a8fffac118528adc5b9b
Joselintp <161532926+Joselintp@users.noreply.github.com>
2024-09-24 21:52:40 -0400

    Update app.module.ts

--- Desafio11-12/educacionit-app/src/app.module.ts
+++ Desafio11-12/educacionit-app/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,11 +10,11 @@ import { MongooseModule } from '@nestjs/mongoose';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_DB_URI, {
-      dbName: process.env.MONGO_DB_NAME,
-      user: process.env.MONGO_DB_USER,
-      pass: process.env.MONGO_DB_PASS,
-    }),
+    // MongooseModule.forRoot(process.env.MONGO_DB_URI, {
+    //  dbName: process.env.MONGO_DB_NAME,
+    //  user: process.env.MONGO_DB_USER,
+    //  pass: process.env.MONGO_DB_PASS,
+    //}),
   ],
   controllers: [AppController],
   providers: [AppService],
